4464d8ec54c118c991b8eb8abdbb3a3644028151
arom4aa-sys <arom4aa@gmail.com>
2026-09-13 17:18:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 import os
 import random
 
 app = Flask(__name__)
+CORS(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
